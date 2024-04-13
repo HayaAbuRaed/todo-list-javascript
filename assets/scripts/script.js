57cc9a167ae1289b0cf62b461a7b1a913450dbda
Haya Abu-Raed <hayaa.aburaed@gmail.com>
2024-04-13 22:44:36 +0200

    implement the addittion functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,6 +4,9 @@ let tasks = [];
 
 const tableBody = document.querySelector(".tasks-table tbody");
 const totalTasks = document.getElementById("total");
+const msg = document.getElementById("msg");
+const addTaskButton = document.getElementById("add-button");
+const newTaskField = document.getElementById("new-task");
 
 // fetch the data from the API
 const fetchTasks = async () => {
@@ -52,4 +55,51 @@ const renderTableRows = (tasks) => {
 // update the total tasks count
 const updateTotal = () => {
   totalTasks.innerHTML = tasks.length;
+};
+
+// add a new task
+addTaskButton.addEventListener("click", () => {
+  const newTaskValue = newTaskField.value.trim();
+
+  if (newTaskValue === "") {
+    showMessage("Please enter a task...", false);
+    return;
+  }
+
+  const newTask = {
+    id: tasks.length + 1,
+    todo: newTaskValue,
+    userId: Math.floor(Math.random() * 100),
+    completed: false,
+  };
+
+  tasks.push(newTask);
+
+  renderTableRows(tasks);
+
+  updateTotal();
+
+  newTaskField.value = "";
+
+  showMessage("Task added successfully ✅");
+});
+
+/**
+ * Display a message in the UI.
+ *
+ * @param {string} message - The message to display.
+ * @param {boolean} success - Whether the message indicates success (default: true).
+ * @returns {void}
+ */
+const showMessage = (message, success = true) => {
+  msg.innerHTML = message;
+
+  msg.classList.add(success ? "success" : "error");
+  newTaskField.classList.add(success ? "success" : "error");
+
+  setTimeout(() => {
+    msg.innerHTML = "";
+    msg.classList.remove("success", "error");
+    newTaskField.classList.remove("success", "error");
+  }, 3000);
 };
